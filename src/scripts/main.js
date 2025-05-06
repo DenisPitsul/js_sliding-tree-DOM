cc4101ca0d41e479de7eb3bfbcd7a0c82cc01b12
Denis Pitsul <denia.pitsul@gmail.com>
2025-05-06 14:01:53 +0300

    add sliding tree DOM solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const treeListElement = document.querySelector('.tree');
+
+treeListElement.querySelectorAll('li').forEach((li) => {
+  li.childNodes.forEach((node) => {
+    if (node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== '') {
+      const span = document.createElement('span');
+
+      span.textContent = node.textContent.trim();
+      li.prepend(span);
+      li.removeChild(node);
+    }
+  });
+});
+
+treeListElement.addEventListener('click', (e) => {
+  const listElement = e.target.closest('li');
+
+  if (!listElement) {
+    return;
+  }
+
+  const innerListElement = listElement.querySelector('ul');
+
+  if (!innerListElement) {
+    return;
+  }
+
+  const isVisible = getComputedStyle(innerListElement).display === 'block';
+
+  innerListElement.style.display = isVisible ? 'none' : 'block';
+});
